d6ec9e55efb908395259cb62a8e2343d411d2824
ashleybot <e.a.greer@gmail.com>
2014-12-06 17:57:50 -0500

    header, footer, and layout

--- registration/header.php
+++ registration/header.php
@@ -1,10 +1,32 @@
-<div id="wrapper">
-	<div id="container">
-    
-    <div id="header" class="no-print">
-    	<div class="container">
-        	<h1>Kentucky Community College - Women in Computing</h1>
-            <div style="clear:both;"></div> 
-           <div style="clear:both;"></div> 
-			</div><!--end container-->
-        </div><!--end header-->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <!--<div class="container">-->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">KYCC-WIC</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/agenda/">Agenda</a></li>
+        <li class="active"><a href="/registration/">Registration</a></li>
+        <li><a href="/hotel/">Hotel</a></li>
+        <li><a href="/sponsors/">Sponsors</a></li>
+        <li><a href="/photos/">2013 Conference Photos</a></li>
+        <li><a href="/faqs/">FAQ</a></li>
+        <li><a href="/contact/">Contact</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  <!--</div>-->
+</nav>
+<div class="bs-doc-header" id="masthead">
+  <img src="../images/logo-final.svg" height="150">
+  <h1>Women in Computing Celebration</h1>
+</div>
+<div id="content">
+  <div class="container">
+    <div class="col-md-12">
