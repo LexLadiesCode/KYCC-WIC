a986c3ba8519dc3997794271f9ce5bf7fb893460
ashleybot <e.a.greer@gmail.com>
2014-12-22 16:29:15 -0500

    updated footer

--- registration/footer.php
+++ registration/footer.php
@@ -1,12 +1,29 @@
             </div><!--/#col-md-12-->
           </div><!--/#container-->
         </div><!--/#content-->
+        
         <div id="footer">
           <div id="footer-top">
+            
+          
+            <img src="../images/sponsors/acmw.jpg" width="150"/>
+          
+            <img src="../images/sponsors/microsoft.png" width="150"/>
+          
+            <img src="../images/sponsors/qxnet.jpg" width="150"/>
+          
+            <img src="../images/sponsors/raytheon.jpg" width="150"/>
+          
+            <img src="../images/sponsors/wic_students.gif" width="150"/>
+          
           </div>
           <div id="footer-bottom">
+            <div class="byline">
+              Site by <a href="http://lexladiescode.org">LexLadiesCode <img src="/images/sponsors/lexladiescode.png" width="15"></a><br />  Registration by Thomas Papanicolaou, BCTC
+            </div>
           </div>
         </div>
+        
       </div><!--/#container-->
     </body>
 </html>
